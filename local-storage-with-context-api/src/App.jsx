456345b4973e5da6fe840c877806ad12fd45e7b8
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-03-20 12:59:51 +0530

    completed context api functionality now local storage functionality start

--- local-storage-with-context-api/src/App.jsx
+++ local-storage-with-context-api/src/App.jsx
@@ -1,12 +1,39 @@
+import { TodoProvider } from "./context"
 
 
 function App() {
 
+  const [todos, setTodo] = useState([])
+
+  const addTodo = (todo) => {
+    setTodo((prev) => [{ id: Date.now(), todo }, ...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodo((prev) => prev.map((prevTodo) => (prevTodo.id === id ? prev : prevTodo)))
+  }
+  const deleteTodo = (id) => {
+    setTodo((prev) => prev.filter((preVal) => (preVal.id !== id)))
+  }
+  const toggleComplete = (id) => {
+    setTodo((prev) => prev.map((preVal) => preVal.id === id ? { ...preVal, completed: !preVal.completed } : preVal))
+  }
+
 
   return (
-    <>
-      <div className="bg-blue-500"><h1>Local Storage with Context Api</h1></div>
-    </>
+    <TodoProvider vlue={{ todos, addTodo, deleteTodo, updateTodo, deleteTodo, toggleComplete }}>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
   )
 }
 
---
+++ local-storage-with-context-api/src/context/TodoContext.js
@@ -0,0 +1,20 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    Todos: [
+        {
+            id: 1,
+            todo: "todo msg",
+            completed: false
+        }
+    ],
+    addTodo: (todo) => { }
+    updatedTodo: (id, todo) => { }
+})
+
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const Todoprovider = TodoContext.Provider
